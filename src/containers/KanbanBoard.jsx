6b7934f3e35d275049b04f122562a373f09f353e
Jesús Diego Velázquez Sánchez <jesu10137@gmail.com>
2026-06-15 18:03:27 +0000

    MGK-22 Como usuario, quiero poder exportar mis tareas en excel. #done

--- src/containers/KanbanBoard.jsx
+++ src/containers/KanbanBoard.jsx
@@ -1,13 +1,65 @@
 import React, { useState } from 'react';
 import MyButton from '../components/MyButton';
 import TaskColumn from './TaskColumn';
-import styles from '../styles/KanbanBoard.module.css'
-
+import styles from '../styles/KanbanBoard.module.css';
+import axios from 'axios';
+/* Librería para exportar a excel */
+import ExcelJS from 'exceljs';
+/* Permitir guardar el archivo */
+import { saveAs } from 'file-saver';
 
 
 function KanbanBoard() {
-    const handleExportButtonClick = () => {
-        alert('Funcionalidad de exportar a Excel no implementada aún');
+const handleExportButtonClick = async () => {
+        try {
+            /* Regresamos objetos de respuesta de axios de todas las tareas, los cuales continen los datos en el .data */
+            const [resPorHacer, resEnProgreso, resTerminado] = await Promise.all([
+                axios.get('/api/tareas/Por hacer'),
+                axios.get('/api/tareas/En progreso'),
+                axios.get('/api/tareas/Terminado')
+            ]);
+
+            /* Metemos las tareas en un solo arreglo (solamente los datos/contenido de la respuesta)*/
+            const todasLasTareas = [
+                ...resPorHacer.data,
+                ...resEnProgreso.data,
+                ...resTerminado.data
+            ];
+
+            /* Creamos el libro y la hoja de excel */
+            const workbook = new ExcelJS.Workbook();
+            const worksheet = workbook.addWorksheet('Mis Tareas');
+
+           /* Definimos las columnas */
+           /*
+              - header: El texto que leerá el usuario en la primera fila.
+              - key: El nombre exacto de la propiedad en nuestro objeto (tarea.id, tarea.titulo) para que Excel sepa qué dato mapear.
+              - width: El ancho visual de la celda en el archivo final.
+            */
+            worksheet.columns = [
+                { header: 'Título', key: 'titulo', width: 30 },
+                { header: 'Descripción', key: 'descripcion', width: 40 },
+                { header: 'Estado', key: 'estado', width: 15 }
+            ];
+
+            /* Agregamos los datos */
+            todasLasTareas.forEach(tarea => {
+                /* addRow(datos, style?: string | undefined) */
+                worksheet.addRow({
+                    titulo: tarea.titulo,
+                    descripcion: tarea.descripcion,
+                    estado: tarea.estado
+                });
+            });
+
+            /* Generamos el archivo y lo descargamos */
+            const buffer = await workbook.xlsx.writeBuffer();
+            saveAs(new Blob([buffer]), 'Reporte_Kanban.xlsx');
+
+        } catch (error) {
+            console.error("Error al exportar a Excel:", error);
+            alert("Hubo un error al intentar generar el archivo Excel.");
+        }
     }
 
     /* Esta es una variable sencilla entera que nos ayudará a refrescar las columnas cada vez que se cambie el estado.*/
@@ -19,9 +71,13 @@ function KanbanBoard() {
 
     return (
         <div className={styles.contenedorPrincipal}>
-            <h1>Kanban Board</h1>
+        <div className={styles.contenedorTitulo}>
+            <h1 className={styles.tituloTablero}>Kanban Board</h1>
+        </div>
+            <div className={styles.botonExportar}>
             <MyButton texto="Exportar a Excel" onClick={handleExportButtonClick} color="secondary"/>
-            <div className={styles.tablero}>
+            </div>
+        <div className={styles.tablero}>
             <TaskColumn 
                 estado="Por hacer" 
                 refreshTrigger={refreshTrigger} 
